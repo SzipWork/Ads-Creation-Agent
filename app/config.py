5b383a011b0994f2d76c4d8d3d065eeeef87fbe3
Souparna  Choudhury <schoudhury7.work@gmail.com>
2026-01-28 17:29:12 +0530

    Delete app/config.py

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-TIKTOK_CLIENT_ID = os.getenv("TIKTOK_CLIENT_ID")
-TIKTOK_CLIENT_SECRET = os.getenv("TIKTOK_CLIENT_SECRET")
